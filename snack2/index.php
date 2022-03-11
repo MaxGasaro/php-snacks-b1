eb9cff38b2f32b4e600c3dfb1e9c17d76cd2ed5f
Massimiliano Gasaro <massimilianogasaro95@gmail.com>
2022-03-11 01:14:28 +0100

    snack2 completed

--- snack2/index.php
+++ snack2/index.php
@@ -1,1 +1,11 @@
 <?php
+ 
+ $name = $_GET['name'];
+ $email = $_GET['mail'];
+ $eta = $_GET['age'];
+
+ if (strlen($name) > 3 && strpos($email, '@') !== false && strpos($email, '.') !== false && is_numeric($eta)) {
+     echo "Accesso riuscito!";
+ } else {
+     echo "Accesso negato!";
+ }
