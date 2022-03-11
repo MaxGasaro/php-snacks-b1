017e425d741e6c96bae60193de40f17ada6b2db6
Massimiliano Gasaro <massimilianogasaro95@gmail.com>
2022-03-11 10:33:10 +0100

    miglioramento snack5 con correzione explode

--- snack2/index.php
+++ snack2/index.php
@@ -1,11 +1,23 @@
 <?php
- 
- $name = $_GET['name'];
- $email = $_GET['mail'];
- $eta = $_GET['age'];
 
- if (strlen($name) > 3 && strpos($email, '@') !== false && strpos($email, '.') !== false && is_numeric($eta)) {
-     echo "Accesso riuscito!";
- } else {
-     echo "Accesso negato!";
- }
+/*
+$name = $_GET['name'];
+$email = $_GET['mail'];
+$eta = $_GET['age'];
+*/
+// se definisco le variabili in questo caso quando eseguo il codice mi da un warning nel caso ci fossero dei valori vuoti
+// perchè comunque cerca di definire la variabile assegnata
+
+if (strlen($_GET['name']) > 3 && strpos($_GET['mail'], '@') !== false && strpos($email, '.') !== false && is_numeric($_GET['age'])) {
+
+    echo "Accesso riuscito!";
+
+} elseif (empty($_GET['name']) || empty($_GET['mail']) || empty($_GET['age'])) {
+
+    echo "Accesso negato!";
+
+} else {
+
+    echo ' Accesso negato!';
+
+}
--- snack5/index.php
+++ snack5/index.php
@@ -2,6 +2,12 @@
 
 $paragrafo = 'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-$sentences = preg_split('/.*?[?.!]+\s+/', $paragrafo);
+//$sentences = preg_split('/.*?[?.!]+\s+/', $paragrafo);
 
-var_dump($sentences);
+$sentences = explode('.', $paragrafo);
+
+//var_dump($sentences); NO!!! si usa solo durante le fasi di debug
+
+for ($i = 0; $i < count($sentences); $i++) {
+    echo "<p> $sentences[$i] </p>"; //giusto usare apici doppi che funzionano come i backtick in js, con apici singoli avrei dovuto fare concatenazione con '.'
+}
